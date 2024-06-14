cb15a25b062a3983ca4fbc19815aa926a8ab1245
ArthurT01 <arthurgozi1@gmail.com>
2024-06-14 16:58:12 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Aplicação_API
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCep = new System.Windows.Forms.TextBox();
+            this.txtRua = new System.Windows.Forms.TextBox();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.txtUf = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCep
+            // 
+            this.txtCep.Location = new System.Drawing.Point(138, 62);
+            this.txtCep.Name = "txtCep";
+            this.txtCep.Size = new System.Drawing.Size(163, 20);
+            this.txtCep.TabIndex = 0;
+            // 
+            // txtRua
+            // 
+            this.txtRua.Location = new System.Drawing.Point(138, 104);
+            this.txtRua.Name = "txtRua";
+            this.txtRua.Size = new System.Drawing.Size(346, 20);
+            this.txtRua.TabIndex = 1;
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Location = new System.Drawing.Point(138, 130);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(346, 20);
+            this.txtBairro.TabIndex = 2;
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Location = new System.Drawing.Point(138, 156);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(258, 20);
+            this.txtCidade.TabIndex = 3;
+            // 
+            // txtUf
+            // 
+            this.txtUf.Location = new System.Drawing.Point(432, 156);
+            this.txtUf.Name = "txtUf";
+            this.txtUf.Size = new System.Drawing.Size(52, 20);
+            this.txtUf.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(97, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "CEP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(58, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Longadouro:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(88, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Bairro:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(82, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Cidade:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(402, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "UF:";
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Location = new System.Drawing.Point(376, 59);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(108, 23);
+            this.btnPesquisar.TabIndex = 11;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 241);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUf);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.txtRua);
+            this.Controls.Add(this.txtCep);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCep;
+        private System.Windows.Forms.TextBox txtRua;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.TextBox txtUf;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnPesquisar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aplicação_API
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void busca(string cep)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://viacep.com.br/ws/" + cep + "/json/");
+            request.AllowAutoRedirect = false;
+            HttpWebResponse ChecaServidor = (HttpWebResponse)request.GetResponse();
+
+            if (ChecaServidor.StatusCode != HttpStatusCode.OK)
+            {
+                MessageBox.Show("Erro na requisição: " + ChecaServidor.StatusCode.ToString());
+                return; // Encerra o códigoz
+            }
+
+            using (Stream webStream = ChecaServidor.GetResponseStream())
+            {
+                if (webStream != null)
+                {
+                    using (StreamReader responseReader = new StreamReader(webStream))
+                    {
+                        String response = responseReader.ReadToEnd();
+                        // MessageBox.Show(response);
+                        response = Regex.Replace(response, "[{},]", string.Empty);
+                        response = response.Replace("\"", "");
+                        // MessageBox.Show(response);
+
+                        String[] substrings = response.Split('\n');
+
+                        int cont = 0;
+                        foreach (var substring in substrings)
+                        {
+                            // CEP
+                            if (cont == 1)
+                            {
+                                string[] valor = substring.Split(':');
+                                txtCep.Text = valor[1].ToString();
+                            }
+
+                            // Logradouro
+                            if (cont == 2)
+                            {
+                                string[] valor = substring.Split(':');
+                                txtRua.Text = valor[1].ToString();
+                            }
+
+
+                            // Bairro
+                            if (cont == 4)
+                            {
+                                string[] valor = substring.Split(':');
+                                txtBairro.Text = valor[1].ToString();
+                            }
+
+                            // Cidade
+                            if (cont == 5)
+                            {
+                                string[] valor = substring.Split(':');
+                                txtCidade.Text = valor[1].ToString();
+                            }
+
+                            // UF
+                            if (cont == 6)
+                            {
+                                string[] valor = substring.Split(':');
+                                txtUf.Text = valor[1].ToString();
+                            }
+                            cont++;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            //executaBuscaBasica();
+            //executaBusca();
+            executaBuscaTratada();
+        }
+
+        private void executaBuscaBasica()
+        {
+            int c = Convert.ToInt32(txtCep.Text);
+
+            BuscaCep BuscaCep = new BuscaCep(c);
+
+            txtRua.Text = BuscaCep.Rua;
+            txtBairro.Text = BuscaCep.Bairro;
+            txtCidade.Text = BuscaCep.Cidade;
+            txtUf.Text = BuscaCep.Estado;
+        }
+
+        private void executaBusca()
+        {
+            string entrada = txtCep.Text;
+            int c;
+
+            if(int.TryParse(entrada, out c))
+            {
+                BuscaCep BuscaCep = new BuscaCep(c);
+
+                txtRua.Text = BuscaCep.Rua;
+                txtBairro.Text = BuscaCep.Bairro;
+                txtCidade.Text = BuscaCep.Cidade;
+                txtUf.Text = BuscaCep.Estado;
+            }
+            else
+            {
+                MessageBox.Show("Por favor, insira um CEP válido que contenha apenas números.",
+                                "Entrada Inválida",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtCep.Clear();
+                txtCep.Focus();
+            }
+        }
+
+        private void executaBuscaTratada()
+        {
+            string input = txtCep.Text;
+            int c;
+
+            if (int.TryParse(input, out c) && input.Length == 8)
+            {
+                try
+                {
+                    BuscaCep BuscaCep = new BuscaCep(c);
+
+                    if(string.IsNullOrEmpty(BuscaCep.Cep))
+                    {
+                        MessageBox.Show("CEP não encontrado. " +
+                                        "Verifique o número digitado e tente novamente.",
+                                        "CEP Inválido", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        txtRua.Text = BuscaCep.Rua;
+                        txtBairro.Text = BuscaCep.Bairro;
+                        txtCidade.Text = BuscaCep.Cidade;
+                        txtUf.Text = BuscaCep.Estado;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("Ocorreu um erro ao buscar o CEP: " + ex.Message,
+                                    "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                              
+            }
+            else
+            {
+                MessageBox.Show("Por favor, insira um CEP válido que contenha apenas números.",
+                                "Entrada Inválida", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtCep.Clear();
+                txtCep.Focus();
+            }
+        }
+    }
+}
